2fa87ae88e4a6aed37c9c3f7fc8f2ca8d5a6f088
syed shayan haider <haidersyedshayan@gmail.com>
2025-09-09 01:51:51 +0500

    Add supertest-automation folder with test files

---
+++ supertest-automation/api.test.js
@@ -0,0 +1,16 @@
+// api.test.js
+const request = require('supertest');
+
+const BASE_URL = 'https://jsonplaceholder.typicode.com';
+
+describe('JSONPlaceholder API Tests', () => {
+  test('GET /posts/1 should return a single post with id = 1', async () => {
+    const response = await request(BASE_URL).get('/posts/1');
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveProperty('id', 1);
+    expect(response.body).toHaveProperty('title');
+    expect(response.body).toHaveProperty('body');
+    expect(response.body).toHaveProperty('userId');
+  });
+});
