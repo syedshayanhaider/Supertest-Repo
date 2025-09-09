b3663e23e0ccf6e9d8189a4efa18f51e6f212b5b
syed shayan haider <haidersyedshayan@gmail.com>
2025-09-09 19:40:26 +0500

    refactor: run api.test.js

--- supertest-automation/api.test.js
+++ supertest-automation/api.test.js
@@ -1,4 +1,4 @@
-// api.test.js
+
 const request = require('supertest');
 
 const BASE_URL = 'https://jsonplaceholder.typicode.com';
@@ -10,7 +10,8 @@ describe('JSONPlaceholder API Tests', () => {
     expect(response.statusCode).toBe(200);
     expect(response.body).toHaveProperty('id', 1);
     expect(response.body).toHaveProperty('title');
-    expect(response.body).toHaveProperty('body');
+    expect(response.body).toHaveProperty('body'); 
     expect(response.body).toHaveProperty('userId');
   });
 });
+
